50e98c2196ae8d4e6a43eba7ef3d3c3c1b546abf
daspro-obsheet2 <wildanwibisono52@gmail.com>
2024-12-10 16:19:23 +0700

    fungsi main - Kelompok 8

--- cafeBumi8.java
+++ cafeBumi8.java
@@ -8,4 +8,37 @@ public class cafeBumi8 {
                                7000, 10000, 13000, 8000, 10000};
     static String[][] pesanan = new String[100][5];
     static int jumlahPesanan = 0;
+
+    public static void main(String[] args) {
+        boolean menu = true;
+
+        while (menu) {
+            System.out.println("\n=========== CAFE BUMI 8 ===========");
+            System.out.println("1. Transaksi Pesanan");
+            System.out.println("2. Tampilkan Daftar Pesanan");
+            System.out.println("3. Total Pendapatan Harian");
+            System.out.println("4. Keluar");
+            System.out.println("===================================");
+            System.out.print("Masukkan Pilihan: ");
+            int pilihan = kel8.nextInt();
+
+            switch (pilihan) {
+                case 1:
+                    prosesPesanan();
+                    break;
+                case 2:
+                    tampilkanPesanan();
+                    break;
+                case 3:
+                    hitungTotalPendapatan();
+                    break;
+                case 4:
+                    menu = false;
+                    System.out.println("\nTerima kasih! Silahkan datang kembali.");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan coba lagi.");
+            }
+        }
+    }
 }
