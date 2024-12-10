9d53407274498677f9bf11a9528fe2354c171f75
maulidaprina <mldaprna@gmail.com>
2024-12-10 16:43:09 +0700

    Fungsi tampilkanPesanan

--- cafeBumi8.java
+++ cafeBumi8.java
@@ -27,7 +27,7 @@ public class cafeBumi8 {
                     prosesPesanan();
                     break;
                 case 2:
-                    //tampilkanPesanan();
+                    tampilkanPesanan();
                     break;
                 case 3:
                     //hitungTotalPendapatan();
@@ -107,5 +107,36 @@ public class cafeBumi8 {
             System.out.println((i + 1) + ". " + daftarMenu[i] + " - Rp " + hargaMenu[i]);
         }
         System.out.println("===================================");
+    }
+
+    public static void tampilkanPesanan() {
+        System.out.println("\n========== DAFTAR PESANAN ==========");
+        for (int i = 0; i < jumlahPesanan; i++) {
+            boolean sudahDitampilkan = false;
+
+            for (int j = 0; j < i; j++) {
+                if (pesanan[i][0].equals(pesanan[j][0]) && pesanan[i][1].equals(pesanan[j][1])) {
+                    sudahDitampilkan = true;
+                    break;
+                }
+            }
+
+            if (!sudahDitampilkan) {
+                System.out.println("Nama Pelanggan: " + pesanan[i][0]);
+                System.out.println("Nomor Meja: " + pesanan[i][1]);
+                System.out.println("\nDetail Pesanan:");
+
+                int totalHargaPelanggan = 0;
+                for (int j = i; j < jumlahPesanan; j++) {
+                    if (pesanan[i][0].equals(pesanan[j][0]) && pesanan[i][1].equals(pesanan[j][1])) {
+                        System.out.println("- " + pesanan[j][2] + " x " + pesanan[j][3] + " = Rp " + pesanan[j][4]);
+                        totalHargaPelanggan += Integer.parseInt(pesanan[j][4]);
+                    }
+                }
+
+                System.out.println("\nTotal Harga Pesanan: Rp " + totalHargaPelanggan);
+                System.out.println("-----------------------------------");
+            }
+        }
     }
 }
