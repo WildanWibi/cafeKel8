007b60b0303739111e12d1d3abaca6e4151a2b70
daspro-obsheet2 <wildanwibisono52@gmail.com>
2024-12-10 16:16:58 +0700

    variabel - Kelompok 8

---
+++ cafeBumi8.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class cafeBumi8 {
+    static Scanner kel8 = new Scanner(System.in);
+    static String[] daftarMenu = {"Roti Bakar Coklat", "Roti Bakar Keju", "Kentang Goreng", "Mie Goreng Spesial", "Nasi Goreng Mawut",
+                                   "Cappucino", "Espresso", "Greentea", "Teh Tarik", "Susu Coklat"};
+    static int[] hargaMenu = {12000, 12000, 10000, 15000, 15000,
+                               7000, 10000, 13000, 8000, 10000};
+    static String[][] pesanan = new String[100][5];
+    static int jumlahPesanan = 0;
+}
